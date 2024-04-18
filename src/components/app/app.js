89df93a846b249db2684cc070f40454a7b54dc76
Dmitriy <mr.dima337@gmail.com>
2024-04-18 15:18:52 +0300

    feat: implement search-panel component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,14 @@
 import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
 import './app.css';
 
 function App() {
     return (
         <div className="app">
             <AppInfo />
+            <div className="search-panel">
+                <SearchPanel />
+            </div>
         </div>
     );
 }
