f570643b28aa8b4b915f59a202f877cd5555e26a
Dmitriy <mr.dima337@gmail.com>
2024-04-21 14:12:26 +0300

    feat: implement color for text employe who will got bonus

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,27 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
+
 function App() {
+
+    const data = [
+        {
+            name: "Jhon Week",
+            salary: 9000,
+            increase: true,
+        },
+        {
+            name: "Jozzeppe Alkopone",
+            salary: 11000,
+            increase: false,
+        },
+        {
+            name: "Albert McConald",
+            salary: 8100,
+            increase: false,
+        },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -16,10 +36,37 @@ function App() {
                 <SearchPanel />
                 <AppFilter />
             </div>
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// function WhoAmI({ name, surname, link }) {
+//     return (
+//         <div>
+//             <h1>My name is {name.firstName}, surname - {surname} </h1>
+//             <a href={link}>My profile</a>
+//         </div>
+//     )
+// }
+
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <WhoAmI name={{ firstName: 'John' }} surname="Smith" link="facebook.com" />
+//             <WhoAmI name={{ firstName: 'Alex' }} surname="Savin" link="vk.com" />
+//         </div>
+//     )
+// }
